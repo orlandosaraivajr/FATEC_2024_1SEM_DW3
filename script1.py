b34248d1ff5cc35cdd9deda84273d84c303ea5e7
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-02-22 20:06:19 -0300

    Script 1

---
+++ script1.py
@@ -0,0 +1,27 @@
+# Sequencia
+print('oi mundo')
+
+print('passo 2')
+
+# Seleção ( IF )
+valor1 = 5
+
+if valor1 > 10:
+    print("Valor maior que dez")
+
+if valor1 > 2:
+    print("Valor maior que dois")
+
+# Iterações com objeto iterável
+lista = list(range(1, 10))
+
+for irineu in lista:
+    print("Achei: ")
+    print(irineu)
+    
+
+lista = list(range(1,10))
+lista.append('Leite')
+lista.append(list(range(1,10)))
+lista.append('Café')
+
