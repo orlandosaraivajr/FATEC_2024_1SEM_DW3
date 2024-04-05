10ac1fa73151361dd0f96a800c525019d331cceb
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-04-04 22:01:49 -0300

    Feriado v.3

--- projeto_feriado/feriados/core/views.py
+++ projeto_feriado/feriados/core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 def natal(request):
-    return render(request, 'natal.html')
+    rua = 'Rua 10'
+    contexto = {'natal':False, 'carnaval':False,
+                'onde_noel_vai':rua}
+    return render(request, 'natal.html', contexto)
 
 def carnaval(request):
     return render(request, 'carnaval.html')
