afe940ae04e37b9b5b002a79b36bc78ba72f4be6
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2024-04-11 22:23:03 -0300

    Django com ORM

--- projeto_feriado/feriados/core/tests.py
+++ projeto_feriado/feriados/core/tests.py
@@ -17,3 +17,27 @@ class CarnavalTest(TestCase):
     
     def test_url_ok(self):
         self.assertEqual(self.resp.status_code, 200)
+
+
+from .models import FeriadoModel
+from datetime import datetime
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+        nome=self.feriado,
+        dia=self.dia,
+        mes=self.mes)
+        self.cadastro.save()  
+
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime) 
+    
+    def test_nome_feriado(self):
+        self.assertEqual(self.cadastro.nome, 'Natal')
